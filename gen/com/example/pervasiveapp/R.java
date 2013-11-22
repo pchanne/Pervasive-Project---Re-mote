1404b9c896082b8c61c744f4930321d5d59327f2
Aditya Ratnaparkhe <aratnapa@indiana.edu>
2013-11-21 22:11:19 -0500

    Implemented File reading

--- gen/com/example/pervasiveapp/R.java
+++ gen/com/example/pervasiveapp/R.java
@@ -24,18 +24,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
+        public static final int ReaderTextView=0x7f090003;
+        public static final int action_settings=0x7f090004;
         public static final int enterFileLabel=0x7f090001;
         public static final int fileNameText=0x7f090000;
         public static final int openFileButton=0x7f090002;
     }
     public static final class layout {
         public static final int activity_filehandler=0x7f030000;
-        public static final int activity_splash=0x7f030001;
+        public static final int activity_reader=0x7f030001;
+        public static final int activity_splash=0x7f030002;
     }
     public static final class menu {
         public static final int file_handler=0x7f080000;
-        public static final int splash=0x7f080001;
+        public static final int reader=0x7f080001;
+        public static final int splash=0x7f080002;
     }
     public static final class raw {
         public static final int book=0x7f040000;
@@ -46,6 +49,7 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060004;
         public static final int title_activity_file_handler=0x7f060003;
+        public static final int title_activity_reader=0x7f060005;
     }
     public static final class style {
         /** 
--- src/com/example/pervasiveapp/FileHandler.java
+++ src/com/example/pervasiveapp/FileHandler.java
@@ -8,8 +8,12 @@ import java.io.InputStream;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
@@ -30,6 +34,7 @@ public class FileHandler extends Activity {
 		setContentView(R.layout.activity_filehandler);
 		fileNameTextView = (TextView) findViewById(R.id.fileNameText);
 		openFileButton  = (Button) findViewById(R.id.openFileButton);
+		Log.i("PervasiveApp", "Clicked");
 //		openFileButton.setOnClickListener(new OnClickListener() {
 //			@Override
 //			public void onClick(View arg0) {
@@ -41,22 +46,51 @@ public class FileHandler extends Activity {
 //		});
 	}
 	
-	private void readFile(View view){
-		System.out.println("Clicked!");
-//		InputStream inputStream = getResources().openRawResource(R.raw.book);
-//		ByteArrayOutputStream bstream = new ByteArrayOutputStream();
-//		int i;
-//		try{
-//			i = inputStream.read();
-//			while(i!=-1){
-//				bstream.write(i);
-//				i = inputStream.read();
-//			}
-//			inputStream.close();
-//		}catch(Exception e){
-//			e.printStackTrace();
-//		}
-//		System.out.println(bstream.toString());
+	public void readFile(View view){
+		
+		Log.i("PervasiveApp", "Clicked");
+		//InputStream inputStream = getResources().openRawResource(R.raw.book);
+		
+		int i;
+		InputStream inputStream;
+		ByteArrayOutputStream bstream =new ByteArrayOutputStream();
+		try{
+			inputStream = getResources().openRawResource(getResources().getIdentifier("raw/"+fileNameTextView.getText().toString(), "raw", getPackageName()));
+			i = inputStream.read();
+			while(i!=-1){
+				bstream.write(i);
+				i = inputStream.read();
+			}
+			inputStream.close();
+		}catch(Resources.NotFoundException re){
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		    builder
+		    .setTitle("Deleting a Contact No")
+		    .setMessage("Are you sure?")
+		    .setIcon(android.R.drawable.ic_dialog_alert)
+		    .setPositiveButton("Yes", new DialogInterface.OnClickListener() 
+		    {
+		        public void onClick(DialogInterface dialog, int which) 
+		        {       
+		               //do some thing here which you need
+		    }
+		    });             
+		    builder.setNegativeButton("No", new DialogInterface.OnClickListener() 
+		    {
+		        public void onClick(DialogInterface dialog, int which) 
+		        {   
+		        dialog.dismiss();           
+		        }
+		    });         
+		AlertDialog alert = builder.create();
+		        alert.show();
+		}
+		catch(Exception e){
+			//e.printStackTrace();
+			e.getMessage();
+		}
+		Log.i("PervasiveApp", bstream.toString());
+		//System.out.println(bstream.toString());
 	}
 
 	@Override
